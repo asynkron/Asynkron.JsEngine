358b051d04af7b68231d50a81084ec1f175746f0
Roger Johansson <roger@asynkron.se>
2025-11-16 21:02:04 +0100

    fix async

--- tests/Asynkron.JsEngine.Tests/TypedCpsTransformerTests.cs
+++ tests/Asynkron.JsEngine.Tests/TypedCpsTransformerTests.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Asynkron.JsEngine.Ast;
 using Asynkron.JsEngine.Lisp;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Asynkron.JsEngine.Tests;
 
@@ -37,33 +38,40 @@ public class TypedCpsTransformerTests
 
         var returnStatement = Assert.IsType<ReturnStatement>(Assert.Single(declaration.Function.Body.Statements));
         var newExpression = Assert.IsType<NewExpression>(returnStatement.Expression);
-        var constructor = Assert.IsType<IdentifierExpression>(newExpression.Constructor);
-        Assert.Same(Symbol.Intern("Promise"), constructor.Name);
+        AssertPromiseConstructor(newExpression.Constructor);
 
         var executor = Assert.IsType<FunctionExpression>(Assert.Single(newExpression.Arguments));
         var tryStatement = Assert.IsType<TryStatement>(Assert.Single(executor.Body.Statements));
-        var expressionStatement = Assert.IsType<ExpressionStatement>(
-            Assert.Single(tryStatement.TryBlock.Statements));
-        var thenCall = Assert.IsType<CallExpression>(expressionStatement.Expression);
-        var member = Assert.IsType<MemberExpression>(thenCall.Callee);
-        var awaitHelperCall = Assert.IsType<CallExpression>(member.Target);
-        var helperIdentifier = Assert.IsType<IdentifierExpression>(awaitHelperCall.Callee);
-        Assert.Same(Symbol.Intern("__awaitHelper"), helperIdentifier.Name);
+        var promiseReturn = Assert.IsType<ReturnStatement>(Assert.Single(tryStatement.TryBlock.Statements));
+        var catchCall = Assert.IsType<CallExpression>(promiseReturn.Expression);
+        var catchMember = Assert.IsType<MemberExpression>(catchCall.Callee);
+        var catchProperty = Assert.IsType<LiteralExpression>(catchMember.Property);
+        Assert.Equal("catch", catchProperty.Value);
 
-        var thenProperty = Assert.IsType<LiteralExpression>(member.Property);
+        var thenInvocation = Assert.IsType<CallExpression>(catchMember.Target);
+        var thenMember = Assert.IsType<MemberExpression>(thenInvocation.Callee);
+        var thenProperty = Assert.IsType<LiteralExpression>(thenMember.Property);
         Assert.Equal("then", thenProperty.Value);
 
-        Assert.Single(thenCall.Arguments);
-        var callback = Assert.IsType<FunctionExpression>(thenCall.Arguments[0].Expression);
+        var awaitHelperCall = Assert.IsType<CallExpression>(thenMember.Target);
+        AssertIdentifierOrMember(awaitHelperCall.Callee, Symbol.Intern("__awaitHelper"), "__awaitHelper");
+
+        Assert.Single(thenInvocation.Arguments);
+        var callback = Assert.IsType<FunctionExpression>(thenInvocation.Arguments[0].Expression);
         var callbackBody = Assert.IsType<BlockStatement>(callback.Body);
-        var callbackExpression = Assert.IsType<ExpressionStatement>(Assert.Single(callbackBody.Statements));
-        var resolveCall = Assert.IsType<CallExpression>(callbackExpression.Expression);
-        var resolveIdentifier = Assert.IsType<IdentifierExpression>(resolveCall.Callee);
-        Assert.Same(Symbol.Intern("__resolve"), resolveIdentifier.Name);
+        var callbackReturn = Assert.IsType<ReturnStatement>(Assert.Single(callbackBody.Statements));
+        var resolveCall = Assert.IsType<CallExpression>(callbackReturn.Expression);
+        AssertIdentifierOrMember(resolveCall.Callee, Symbol.Intern("__resolve"), "__resolve");
+
+        Assert.Single(catchCall.Arguments);
+        var catchCallback = Assert.IsType<FunctionExpression>(catchCall.Arguments[0].Expression);
+        var catchCallbackReturn = Assert.IsType<ReturnStatement>(Assert.Single(catchCallback.Body.Statements));
+        var catchRejectCall = Assert.IsType<CallExpression>(catchCallbackReturn.Expression);
+        AssertIdentifierOrMember(catchRejectCall.Callee, Symbol.Intern("__reject"), "__reject");
     }
 
     [Fact]
-    public async Task AsyncFunctionWithMultipleStatements_IsRejected()
+    public async Task AsyncFunctionWithMultipleStatements_Rewrites()
     {
         const string source = """
             async function fail() {
@@ -76,7 +84,23 @@ public class TypedCpsTransformerTests
         var (_, typedBefore, _) = engine.ParseWithTransformationSteps(source);
         var transformer = new TypedCpsTransformer();
 
-        Assert.Throws<NotSupportedException>(() => transformer.Transform(typedBefore));
+        var transformed = transformer.Transform(typedBefore);
+
+        var declaration = Assert.IsType<FunctionDeclaration>(Assert.Single(transformed.Body));
+        Assert.False(declaration.Function.IsAsync);
+
+        var functionReturn = Assert.IsType<ReturnStatement>(Assert.Single(declaration.Function.Body.Statements));
+        var newExpression = Assert.IsType<NewExpression>(functionReturn.Expression);
+        AssertPromiseConstructor(newExpression.Constructor);
+
+        var executor = Assert.IsType<FunctionExpression>(Assert.Single(newExpression.Arguments));
+        var tryStatement = Assert.IsType<TryStatement>(Assert.Single(executor.Body.Statements));
+        Assert.Contains(tryStatement.TryBlock.Statements, statement => statement is ReturnStatement);
+
+        var catchClause = Assert.IsType<CatchClause>(tryStatement.Catch);
+        var catchReturn = Assert.IsType<ReturnStatement>(Assert.Single(catchClause.Body.Statements));
+        var rejectCall = Assert.IsType<CallExpression>(catchReturn.Expression);
+        AssertIdentifierOrMember(rejectCall.Callee, Symbol.Intern("__reject"), "__reject");
     }
 
     // [Fact]
@@ -109,4 +133,58 @@ public class TypedCpsTransformerTests
     //     var actualSnapshot = TypedAstSnapshot.Create(typedCps);
     //     Assert.Equal(expectedSnapshot, actualSnapshot);
     // }
+
+    private static void AssertPromiseConstructor(ExpressionNode expression)
+    {
+        switch (expression)
+        {
+            case IdentifierExpression identifier:
+                Assert.Same(Symbol.Intern("Promise"), identifier.Name);
+                return;
+            case MemberExpression member when member.Property is LiteralExpression literal &&
+                                              literal.Value is string literalValue &&
+                                              literalValue == "Promise":
+                return;
+            default:
+                throw new XunitException("Expected Promise constructor.");
+        }
+    }
+
+    private static void AssertIdentifierOrMember(ExpressionNode expression, Symbol symbol, string propertyName)
+    {
+        switch (expression)
+        {
+            case IdentifierExpression identifier:
+                Assert.Same(symbol, identifier.Name);
+                break;
+            case MemberExpression member when MatchesMemberName(member.Property, symbol, propertyName):
+                break;
+            case MemberExpression member:
+                var propertyLiteral = member.Property as LiteralExpression;
+                var literalDetail = propertyLiteral?.Value is null
+                    ? string.Empty
+                    : $" value '{propertyLiteral.Value}'";
+                var propertyType = propertyLiteral?.Value is not null
+                    ? $"{member.Property.GetType().Name} ({propertyLiteral.Value.GetType().Name})"
+                    : member.Property.GetType().Name;
+                throw new XunitException(
+                    $"Expected reference to '{propertyName}', but saw member property {propertyType}{literalDetail}.");
+            default:
+                throw new XunitException(
+                    $"Expected reference to '{propertyName}', but saw {expression.GetType().Name}.");
+        }
+    }
+
+    private static bool MatchesMemberName(ExpressionNode property, Symbol symbol, string propertyName)
+    {
+        return property switch
+        {
+            LiteralExpression literal when literal.Value is string literalValue &&
+                                          literalValue == propertyName => true,
+            LiteralExpression literal when literal.Value is Symbol literalSymbol &&
+                                          ReferenceEquals(literalSymbol, symbol) => true,
+            IdentifierExpression identifier when ReferenceEquals(identifier.Name, symbol) => true,
+            _ => false
+        };
+    }
 }

dcbf11a3049569d399e03f66ed0ae3429eae9e44
Roger Johansson <roger@asynkron.se>
2025-12-14 17:09:01 +0100

    ...

--- tests/Asynkron.JsEngine.Tests.Test262/Generated/Tests262Harness.Tests.language.generated.cs
+++ tests/Asynkron.JsEngine.Tests.Test262/Generated/Tests262Harness.Tests.language.generated.cs
@@ -3,7 +3,7 @@
 //   Generated using the Test262Harness v1.0.3.0
 //   Suite Git SHA: a073f479f80b336256b7fc4e04700c827293e2fe
 //   Suite Directory: 
-//   Template SHA: 291626AB62B086EE566A94A92ED312863F5FEE8F156D22BE20DEF752E4931BCB
+//   Template SHA: D01FC9C4958D55D2221DFA403B7101F35DF8A2F7707714D7DB462CB7FC890D85
 //   Command line: /Users/rogerjohansson/.nuget/packages/test262harness.console/1.0.3/tools/net8.0/any/Test262Harness.Console.dll generate
 //   Settings file: /Users/rogerjohansson/git/asynkron/JsEngine2/tests/Asynkron.JsEngine.Tests.Test262/Test262Harness.settings.json
 // </auto-generated>

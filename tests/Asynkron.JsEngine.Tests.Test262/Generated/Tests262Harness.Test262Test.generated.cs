fbca40f3600d816528933d3b04b6a52fc4cd468e
Roger Johansson <roger@asynkron.se>
2025-12-19 14:00:47 +0100

    .

--- tests/Asynkron.JsEngine.Tests.Test262/Generated/Tests262Harness.Test262Test.generated.cs
+++ tests/Asynkron.JsEngine.Tests.Test262/Generated/Tests262Harness.Test262Test.generated.cs
@@ -3,7 +3,7 @@
 //   Generated using the Test262Harness v1.0.3.0
 //   Suite Git SHA: a073f479f80b336256b7fc4e04700c827293e2fe
 //   Suite Directory: 
-//   Template SHA: 04F28E897A5862B075F8222D68643D8F19A7193C2B347F7274A05D1C75503ACC
+//   Template SHA: 9932F6D471D58452A12A0D43404D72009EAA8AEFA5723877A18BA07097869677
 //   Command line:  /Users/rogerjohansson/.nuget/packages/test262harness.console/1.0.3/tools/net8.0/any/Test262Harness.Console.dll generate
 //   Settings file: /Users/rogerjohansson/git/asynkron/Asynkron.JsEngine/tests/Asynkron.JsEngine.Tests.Test262/Test262Harness.settings.json
 // </auto-generated>
--- tests/Asynkron.JsEngine.Tests.Test262/Generated/Tests262Harness.Tests.intl402.generated.cs
+++ tests/Asynkron.JsEngine.Tests.Test262/Generated/Tests262Harness.Tests.intl402.generated.cs
@@ -3,7 +3,7 @@
 //   Generated using the Test262Harness v1.0.3.0
 //   Suite Git SHA: a073f479f80b336256b7fc4e04700c827293e2fe
 //   Suite Directory: 
-//   Template SHA: C89089F481EFDD0319955A0B19C3392827B7EFF45C992A4ED4BA91B444053F1E
+//   Template SHA: 3FA845933C4E861476325BE38C7C17C455D78BE139B819AAF8413FEE4216B6FF
 //   Command line: /Users/rogerjohansson/.nuget/packages/test262harness.console/1.0.3/tools/net8.0/any/Test262Harness.Console.dll generate
 //   Settings file: /Users/rogerjohansson/git/asynkron/Asynkron.JsEngine/tests/Asynkron.JsEngine.Tests.Test262/Test262Harness.settings.json
 // </auto-generated>

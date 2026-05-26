026117840732c21cb32ad48363223af018be761c
Faktorial Agent <faktorial-agent@example.com>
2026-05-26 20:39:13 +0200

    Remove legacy object receiver Intl EnsureBrand overload

--- src/Asynkron.JsEngine/StdLib/Intl/IntlBrandExtensions.cs
+++ src/Asynkron.JsEngine/StdLib/Intl/IntlBrandExtensions.cs
@@ -8,18 +8,6 @@ namespace Asynkron.JsEngine.StdLib.Intl;
 
 internal static class IntlBrandExtensions
 {
-    public static JsObject EnsureBrand(this object? candidate, string brandKey, RealmState realm,
-        string errorMessage)
-    {
-        if (candidate is JsObject obj && obj.TryGetProperty(brandKey, out var marker) &&
-            marker.TryGetBoolean(out var flag) && flag)
-        {
-            return obj;
-        }
-
-        throw StandardLibrary.ThrowTypeError(errorMessage, realm: realm);
-    }
-
     public static JsObject EnsureBrand(this JsValue candidate, string brandKey, RealmState realm,
         string errorMessage)
     {

7a26a000fd049429bd454fb9f6e298ba67e2ffdf
Faktorial Agent <faktorial-agent@example.com>
2026-05-28 18:31:54 +0200

    Add decodeURI four-byte timeout regression coverage

--- tests/Asynkron.JsEngine.Tests.Test262/RegressionTests.cs
+++ tests/Asynkron.JsEngine.Tests.Test262/RegressionTests.cs
@@ -226,6 +226,15 @@ public class RegressionTests
         Assert.That(timeout, Is.EqualTo(TimeSpan.FromSeconds(90)));
     }
 
+    [TestCase("built-ins/decodeURI/S15.1.3.1_A2.5_T1.js")]
+    [TestCase("test/built-ins/decodeURI/S15.1.3.1_A2.5_T1.js")]
+    public void DecodeURIFourByteFixture_UsesExtendedExecutionTimeout(string fileName)
+    {
+        var timeout = Test262Test.GetTest262ExecutionTimeout(fileName);
+
+        Assert.That(timeout, Is.EqualTo(TimeSpan.FromSeconds(90)));
+    }
+
     [TestCase("built-ins/RegExp/character-class-escape-non-whitespace.js")]
     [TestCase("test/built-ins/RegExp/character-class-escape-non-whitespace.js")]
     public void RegExpNonWhitespaceFixture_UsesExtendedExecutionTimeout(string fileName)
